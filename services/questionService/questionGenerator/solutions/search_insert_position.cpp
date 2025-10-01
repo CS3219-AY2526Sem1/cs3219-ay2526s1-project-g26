cac12069a4c52a50cc45652fb2ab6de01bebb9b6
Wu Zengfu <wuzengfu666@gmail.com>
2025-10-01 16:44:46 +0800

    feat: add util repo to generate SQL scripts for questions and test cases

---
+++ services/questionService/questionGenerator/solutions/search_insert_position.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int n, target;
+    cin >> n >> target;
+
+    vector<int> nums(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> nums[i];
+    }
+
+    int left = 0;
+    int right = nums.size() - 1;
+
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+
+        if (nums[mid] == target)
+        {
+            return mid;
+        }
+        else if (nums[mid] > target)
+        {
+            right = mid - 1;
+        }
+        else
+        {
+            left = mid + 1;
+        }
+    }
+
+    cout << left << endl;
+    return 0;
+}
