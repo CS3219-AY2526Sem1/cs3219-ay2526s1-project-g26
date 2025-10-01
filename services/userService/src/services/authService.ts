17cbba8a9855646d7ab0053bb86f0d308eef5f1e
Daniel Lai <e0360577@u.nus.edu>
2025-10-01 20:31:20 +0800

    Fix linting

--- services/userService/src/services/authService.ts
+++ services/userService/src/services/authService.ts
@@ -15,10 +15,10 @@ export const createUser = async (
 
   try {
     // identical to yup internal regex in frontend
-    const emailRegex = 
-      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
-    const passwordRegex = 
-      /^(?=.*[a-z].*)(?=.*[A-Z].*)(?=.*\d.*)(?=.*[!-,:-@[-`{-~].*)[!-~]{8,}$/;
+    const emailRegex =
+      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+    const passwordRegex =
+      /^(?=.*[a-z].*)(?=.*[A-Z].*)(?=.*\d.*)(?=.*[!-,:-@[-`{-~].*)[!-~]{8,}$/
 
     if (
       !email ||
