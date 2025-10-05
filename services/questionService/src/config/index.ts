96bba14617225bb00f4225ddd0a45a686ccf2201
Wu Zengfu <wuzengfu666@gmail.com>
2025-10-05 23:30:38 +0800

    feat: use userService api for token verification

--- services/questionService/src/config/index.ts
+++ services/questionService/src/config/index.ts
@@ -1,4 +1,5 @@
-export const JWT_SECRET = process.env.JWT_SECRET || 'secret'
 export const PORT = process.env.PORT || 4010
 export const DATABASE_URL =
   process.env.DATABASE_URL || 'postgres://user:password@localhost:5432/dbname'
+export const USER_SERVICE_URL =
+  process.env.USER_SERVICES_URL || 'http://user-service:4000'
