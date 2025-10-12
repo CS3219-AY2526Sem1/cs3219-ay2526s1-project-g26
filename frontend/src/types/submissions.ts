8aadd4e2c3cb009c3032aea7690e49d5b4115b29
Daniel Lai <e0360577@u.nus.edu>
2025-10-12 15:49:09 +0800

    Implement basic pagination and UI layout

---
+++ frontend/src/types/submissions.ts
@@ -0,0 +1,8 @@
+export interface SubmissionDataSummary {
+  submission_id: string,
+  title: string,
+  submission_time: string,
+  overall_status: string,
+  difficulty: string,
+  language: string,
+}
