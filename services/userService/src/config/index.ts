48488f1f1a42fee8d76fa2ca51bf6853f05758ee
Wu Zengfu <wuzengfu666@gmail.com>
2025-10-19 16:39:15 +0800

    fix: use more db environment variables instead of a single connection string
    Avoid using connection string in case of special characters in password resulting in the connection string being un-encod-able

--- services/userService/src/config/index.ts
+++ services/userService/src/config/index.ts
@@ -1,5 +1,10 @@
+import dotenv from 'dotenv'
+dotenv.config()
 export const JWT_SECRET = process.env.JWT_SECRET || 'secret'
 export const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || '14d'
 export const PORT = process.env.PORT || 4000
-export const DATABASE_URL =
-  process.env.DATABASE_URL || 'postgres://user:password@localhost:5432/dbname'
+export const DB_PORT = parseInt(process.env.DB_PORT || '5432')
+export const DB_HOST = process.env.DB_HOST || 'localhost'
+export const DB_USER = process.env.DB_USER || 'postgres'
+export const DB_NAME = process.env.DB_NAME || 'user_db'
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'postgres'
