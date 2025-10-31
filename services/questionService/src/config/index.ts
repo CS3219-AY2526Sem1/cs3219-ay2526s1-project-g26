a7144fa8d2c0d978a50ec4bb5cd1abde86940d17
Wu Zengfu <wuzengfu666@gmail.com>
2025-10-31 16:39:24 +0800

    fix: wrong user service url

--- services/questionService/src/config/index.ts
+++ services/questionService/src/config/index.ts
@@ -2,4 +2,4 @@ export const PORT = process.env.PORT || 4010
 export const DATABASE_URL =
   process.env.DATABASE_URL || 'mongodb://question-service-db:27017/questions_db'
 const BACKEND_BASE_URL = process.env.BACKEND_BASE_URL || 'http://localhost'
-export const USER_SERVICE_URL = `${BACKEND_BASE_URL}/api/question`
+export const USER_SERVICE_URL = `${BACKEND_BASE_URL}/api/user`
